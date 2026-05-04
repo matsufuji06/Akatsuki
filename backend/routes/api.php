8fc99cc1e53189f1db56c87972b3125d1f28a004
matsufuji06 <n.matsufuji06@gmail.com>
2026-05-04 19:45:16 +0900

    APIルーティング関連ファイル作成

---
+++ backend/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Routre::prefix('v1')->group(function () {
+    Route::post('/auth/login', [AuthController::class, 'login']);
+});
